59bdbd9d57a5f7fbc6ab9c2a51cd1b4d47f629c9
Yash Saboo <yashsaboo99@gmail.com>
2020-10-30 13:41:37 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(
+ name = 'CS527_Fall20',
+ packages = ['CS527_Fall20'],
+ version = '1.0.0',
+ url = 'https://github.com/yashsaboo/CS527_Fall20/archive/1.0.0.tar.gz'
+)

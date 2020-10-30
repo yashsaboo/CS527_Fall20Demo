d567f94033d5cfba7c2e2d15d04075a734c28523
Yash Saboo <yashsaboo99@gmail.com>
2020-10-30 15:31:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
- name = 'CS527_Fall20',
- packages = ['CS527_Fall20'],
+ name = 'CS527_Fall20Demo',
+ packages = ['CS527_Fall20Demo'],
  version = '1.0.0',
- url = 'https://github.com/yashsaboo/CS527_Fall20/archive/1.0.0.tar.gz'
+ url = 'https://github.com/yashsaboo/CS527_Fall20Demo/archive/1.0.0.tar.gz'
 )
